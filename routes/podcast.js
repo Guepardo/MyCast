99bdb64c23c1994fbe0dc69f28a921151979b251
Guepardo <bsinet@hotmail.com>
2016-08-27 21:14:58 -0300

    primeiro teste com spa

--- routes/podcast.js
+++ routes/podcast.js
@@ -1,4 +1,4 @@
 module.exports = (app) =>{
 	app.post('/api/pod/getAll', app.controllers.podcastController.getAll); 
-	app.post('/api/pod/new', app.controllers.podcastController.new); 
+	app.post('/api/pod/new'   , app.controllers.podcastController.new); 
 }; 
